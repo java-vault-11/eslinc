2fe5e5296d79515c34c9bc7a33183102ba4c8c3e
Luke <luke.huang@linctronix.com>
2016-11-24 17:44:50 +0800

    adding divolt kafka consumer

--- src/main/java/com/linctronix/service/EventServiceApplication.java
+++ src/main/java/com/linctronix/service/EventServiceApplication.java
@@ -1,7 +1,6 @@
 package com.linctronix.service;
 
 import javax.sql.DataSource;
-import org.apache.commons.dbcp2.BasicDataSource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -11,18 +10,5 @@ public class EventServiceApplication {
 	
 	public static void main(String args[]) {
 		SpringApplication.run(EventServiceApplication.class, args);
-	}
-	
-	@Bean
-	DataSource dataSource() {
-	    BasicDataSource dataSourceConfig = new BasicDataSource();
-	    dataSourceConfig.setDriverClassName("org.postgresql.Driver");
-
-	    dataSourceConfig.setUrl("jdbc:postgresql://127.0.0.1:5432/rule_action");
-	    dataSourceConfig.setUsername("luke");
-	    dataSourceConfig.setValidationQuery("SELECT 1");
-	    dataSourceConfig.setPassword("29597808");
-
-	    return dataSourceConfig;
 	}
 }
